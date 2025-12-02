787bd50c833c403e0b5b0b7f01157141d85afe3e
tandw8-blip <tandw8@gmail.com>
2025-12-03 01:34:19 +0800

    added shop-page controller

--- src/api/shop-page/controllers/shop-page.js
+++ src/api/shop-page/controllers/shop-page.js
@@ -6,4 +6,41 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::shop-page.shop-page');
+module.exports = createCoreController('api::shop-page.shop-page', ({ strapi }) => ({
+
+  async find(ctx) {
+    // 1. Get the default single type Shop Page
+    const page = await strapi.entityService.findMany(
+      'api::shop-page.shop-page',
+      {
+        populate: {
+          heroBanner: { populate: "*" },
+          sections: { populate: "*" }
+        }
+      }
+    );
+
+    // 2. Fetch featured shops
+    const featuredShops = await strapi.entityService.findMany(
+      'api::shop.shop',
+      {
+        filters: { isFeatured: true },
+        sort: { sortOrder: 'asc' },
+        populate: {
+          bannerImage: true,
+          logo: true,
+          categories: true,
+          mapMarker: true
+        }
+      }
+    );
+
+    return {
+      data: {
+        ...page,
+        featuredShops
+      }
+    };
+  }
+
+}));
